387e021f7fd0f203e77311532309a6e90d0185b7
sidhujag <sidhujag@hotmail.com>
2015-01-10 00:09:31 -0800

    cleanup and refactor

--- modules/gateways/bitshares/userfunctions.php
+++ modules/gateways/bitshares/userfunctions.php
@@ -4,21 +4,7 @@ require ROOT.'../../../includes/functions.php';
 require ROOT.'../../../includes/gatewayfunctions.php';
 require ROOT.'../../../includes/invoicefunctions.php';
 
-function getOpenOrdersHelper()
-{
-	$openOrderList = array();
-	$result    = mysql_query("SELECT tblinvoices.id, tblinvoices.total, tblinvoices.status, tblcurrencies.code FROM tblinvoices, tblclients, tblcurrencies where tblinvoices.userid = tblclients.id and tblclients.currency = tblcurrencies.id and tblinvoices.status='Unpaid'");
-	while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
-		$newOrder = array();
-		$newOrder['total'] = $row['total'];
-		$newOrder['currency_code'] = $row['code'];
-		$newOrder['order_id'] = $row['id'];
-		$newOrder['date_added'] = 0;
-		array_push($openOrderList,$newOrder);
-	}
-  return $openOrderList;
-}
-function isOrderComplete($memo, $order_id)
+function isOrderCompleteUser($memo, $order_id)
 {
 	global $accountName;
 	global $hashSalt;
@@ -38,7 +24,7 @@ function isOrderComplete($memo, $order_id)
 	}
 	return FALSE;	
 }
-function doesOrderExist($memo, $order_id)
+function doesOrderExistUser($memo, $order_id)
 {
 	global $accountName;
 	global $hashSalt;
@@ -65,151 +51,54 @@ function doesOrderExist($memo, $order_id)
 	}
 	return FALSE;
 }
-
-function completeOrderUser($memo, $order_id)
+function getOpenOrdersUser()
 {
-	global $baseURL;
-	global $accountName;
-	global $rpcUser;
-	global $rpcPass;
-	global $rpcPort;
-	global $demoMode;
-	global $hashSalt;
-	$gatewaymodule = "bitshares";
+	$openOrderList = array();
+	$result    = mysql_query("SELECT tblinvoices.id, tblinvoices.total, tblinvoices.status, tblcurrencies.code FROM tblinvoices, tblclients, tblcurrencies where tblinvoices.userid = tblclients.id and tblclients.currency = tblcurrencies.id and tblinvoices.status='Unpaid'");
+	while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+		$newOrder = array();
+		$newOrder['total'] = $row['total'];
+		$newOrder['currency_code'] = $row['code'];
+		$newOrder['order_id'] = $row['id'];
+		$newOrder['date_added'] = 0;
+		array_push($openOrderList,$newOrder);
+	}
+  return $openOrderList;
+}
+function completeOrderUser($order)
+{
+	$ret = array();
+  $gatewaymodule = "bitshares";
 	$GATEWAY       = getGatewayVariables($gatewaymodule);	
-	$orderArray = getOrder($memo, $order_id);
-	if(count($orderArray) <= 0)
-	{
-	  $ret = array();
-	  $ret['error'] = 'Could not find this order in the system, please review the Order ID and Memo';
-	  return $ret;
-	}
+  
+	# Checks invoice ID is a valid invoice number or ends processing
+	$invoiceid = checkCbInvoiceID($order['order_id'], $GATEWAY["name"]);
 
-	if ($orderArray[0]['order_id'] !== $order_id) {
-		$ret = array();
-		$ret['error'] = 'Invalid Order ID';
-		return $ret;
-	}
-	$demo = FALSE;
-	if($demoMode === "1" || $demoMode === 1 || $demoMode === TRUE || $demoMode === "true")
-	{
-		$demo = TRUE;
-	}
-	$response = btsVerifyOpenOrders($orderArray, $accountName, $rpcUser, $rpcPass, $rpcPort, $hashSalt, $demo);
+	$transid = $order['trx_id'];
+	checkCbTransID($transid); # Checks transaction number isn't already in the database and ends processing if it does
 
-	if(array_key_exists('error', $response))
-	{
-	  $ret = array();
-	  $ret['error'] = 'Could not verify order. Please try again';
-	  return $ret;
-	}
-	$ret = array();	
-	$ret['url'] = $baseURL;
-	foreach ($response as $responseOrder) {
-		switch($responseOrder['status'])
-		{
-			case 'complete':
-			case 'overpayment':   
-				# Checks invoice ID is a valid invoice number or ends processing
-				$invoiceid = checkCbInvoiceID($responseOrder['order_id'], $GATEWAY["name"]);
-
-				$transid = $responseOrder['trx_id'];
-				checkCbTransID($transid); # Checks transaction number isn't already in the database and ends processing if it does
-
-				# Successful
-				$fee = 0;
-				$amount = $responseOrder['amount']; // left blank, this will auto-fill as the full balance
-				addInvoicePayment($invoiceid, $transid, $amount, $fee, $gatewaymodule); # Apply Payment to Invoice	  
-				$ret['url'] = $baseURL.'viewinvoice.php?id='.$order_id;				
-				logTransaction($GATEWAY["name"], $responseOrder, $responseOrder['status']);
-				break;		
-			default:
-				break;	    
-		}		 
-	}
-	
+	# Successful
+	$fee = 0;
+	$amount = $order['amount']; // left blank, this will auto-fill as the full balance
+	addInvoicePayment($invoiceid, $transid, $amount, $fee, $gatewaymodule); # Apply Payment to Invoice	  
+	$ret['url'] = $baseURL.'viewinvoice.php?id='.$order['order_id'];				
+	logTransaction($GATEWAY["name"], $order, $order['status']);
 	return $ret;
 }
-function cancelOrderUser($memo, $order_id)
+function cancelOrderUser($order)
 {
-	global $baseURL;
 	$response = array();
-	$response['url'] = $baseURL;
-	
-	$orderArray = getOrder($memo, $order_id);
-	
-	if(count($orderArray) <= 0)
-	{
-	  return $response;
-	}
-	if ($orderArray[0]['order_id'] !== $order_id)
-	{
-	  return $response;
-	}
-	$res = mysql_query("UPDATE tblinvoices, tblclients, tblcurrencies SET tblinvoices.status='Cancelled' WHERE tblinvoices.userid = tblclients.id AND tblclients.currency = tblcurrencies.id and tblinvoices.id=$order_id");
+	$res = mysql_query("UPDATE tblinvoices, tblclients, tblcurrencies SET tblinvoices.status='Cancelled' WHERE tblinvoices.userid = tblclients.id AND tblclients.currency = tblcurrencies.id and tblinvoices.id=$order['order_id']");
 	if(!$res)
 	{
-		return $response;
+		$response['error'] = 'Could not cancel this order!';
 	}
-	$response['url'] = $baseURL.'viewinvoice.php?id='.$order_id;
+	$response['url'] = $baseURL.'viewinvoice.php?id='.$order['order_id'];
 	return $response;
 }
 function cronJobUser()
 {
-	global $cronToken;
-	global $baseURL;
-	global $accountName;
-	global $rpcUser;
-	global $rpcPass;
-	global $rpcPort;
-	global $demoMode;
-	global $hashSalt;
-	$gatewaymodule = "bitshares";
-	$GATEWAY       = getGatewayVariables($gatewaymodule);	
-	$orderArray = getOpenOrdersHelper();
-	if(count($orderArray) <= 0)
-	{
-	  $ret = array();
-	  $ret['error'] = 'No open orders found!';
-	  return $ret;
-	}
-
-	$demo = FALSE;
-	if($demoMode === "1" || $demoMode === 1 || $demoMode === TRUE || $demoMode === "true")
-	{
-		$demo = TRUE;
-	}
-	$response = btsVerifyOpenOrders($orderArray, $accountName, $rpcUser, $rpcPass, $rpcPort, $hashSalt, $demo);
-
-	if(array_key_exists('error', $response))
-	{
-	  $ret = array();
-	  $ret['error'] = 'Could not verify order. Please try again';
-	  return $ret;
-	}	
-	foreach ($response as $responseOrder) {
-		switch($responseOrder['status'])
-		{
-			case 'complete':    	
-			case 'overpayment':
-				# Checks invoice ID is a valid invoice number or ends processing
-				$invoiceid = checkCbInvoiceID($responseOrder['order_id'], $GATEWAY["name"]);
-
-				$transid = $responseOrder['trx_id'];
-				checkCbTransID($transid); # Checks transaction number isn't already in the database and ends processing if it does
-
-				# Successful
-				$fee = 0;
-				$amount = ''; // left blank, this will auto-fill as the full balance
-				addInvoicePayment($invoiceid, $transid, $amount, $fee, $gatewaymodule); # Apply Payment to Invoice	 
-				logTransaction($GATEWAY["name"], $responseOrder, $responseOrder['status']);
-				break; 
-			default:
-				break;	    
-		}	 
-	}
-
-	return $response;	
+	return 'Success!';
 }
 function createOrderUser()
 {
